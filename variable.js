76cb7312d32a89ee1d2c558cf00f278d5007740a
Zeane26 <zeaneasuit08@gmail.com>
2025-03-15 14:41:41 +0800

    variable.js

---
+++ variable.js
@@ -0,0 +1,12 @@
+let name = "Zeane Asuit"; 
+
+let whatDoYouWannaBecomeInYourLife = "programmer";
+
+let gender = "18";
+
+let twitterHandle = "ZeaneAsuit@gmail.com"; 
+
+console.log("Name: " + name);
+console.log("What you want to become in life: " + whatDoYouWannaBecomeInYourLife);
+console.log("Gender: " + gender);
+console.log("Twitter Handle: " + twitterHandle);
